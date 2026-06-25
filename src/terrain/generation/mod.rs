6af4fff13ba03e5bee1de1cad891a2a2a77c8177
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-06-25 20:49:26 +0400

    Expose world shape sampler

--- src/terrain/generation/mod.rs
+++ src/terrain/generation/mod.rs
@@ -1,7 +1,9 @@
 pub mod config;
 pub mod noise;
 pub mod sdf;
+pub mod world_shape;
 
 pub use config::TerrainConfig;
 pub use noise::{fbm, ridged_fbm, sample_terrain_height};
 pub use sdf::sample_terrain_sdf;
+pub use world_shape::{BiomeHint, OceanClass, WorldShapeConfig, WorldShapeSample, WorldShapeSampler};
