67a3c3f78f5af31d94913dc6e3d009ac89f753ee
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-06-25 21:07:22 +0400

    Expose world shape placement filters

--- src/terrain/generation/mod.rs
+++ src/terrain/generation/mod.rs
@@ -2,6 +2,7 @@ pub mod config;
 pub mod noise;
 pub mod sdf;
 pub mod world_shape;
+pub mod world_shape_placement;
 
 pub use config::TerrainConfig;
 pub use noise::{fbm, ridged_fbm, sample_terrain_height};
@@ -9,4 +10,7 @@ pub use sdf::sample_terrain_sdf;
 pub use world_shape::{
     BiomeHint, CoastSurfaceClass, OceanClass, WorldShapeConfig, WorldShapeSample,
     WorldShapeSampler,
+};
+pub use world_shape_placement::{
+    can_place, placement_rejection, PlacementKind, PlacementRejectReason,
 };
