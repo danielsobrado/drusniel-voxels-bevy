b9eadd1a8db8e008d530bb1086ec01807cab54c2
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-06-26 19:53:11 +0400

    feat(clod-poc): export construction module

---
+++ tools/clod-poc/src/construction/index.ts
@@ -0,0 +1,19 @@
+export { createConstructionController } from "./construction_controller.js";
+export type { ConstructionController, ConstructionControllerDeps, ConstructionControllerStats } from "./construction_controller.js";
+export { defaultConstructionConfig, parseConstructionConfig } from "./config.js";
+export { ConstructionSnapIndex, constructionSnapMath } from "./snap_index.js";
+export { createConstructionCandidate, createFreePlacementPosition, validateConstructionPlacement } from "./placement.js";
+export type {
+  ConstructionCandidate,
+  ConstructionConfig,
+  ConstructionGhostConfig,
+  ConstructionMaterial,
+  ConstructionPieceDef,
+  ConstructionPlacementConfig,
+  ConstructionSnapConfig,
+  ConstructionSnapPoint,
+  ConstructionSnapResult,
+  IndexedConstructionSnapPoint,
+  PlacedConstructionPiece,
+  SnapGroup,
+} from "./types.js";
