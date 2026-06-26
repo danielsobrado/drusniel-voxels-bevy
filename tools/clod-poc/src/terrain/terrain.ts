78a05cc3f45a1c654b4b2e74df691be61bc1ca0d
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-06-26 21:54:01 +0400

    Export terrain SDF helpers

--- tools/clod-poc/src/terrain/terrain.ts
+++ tools/clod-poc/src/terrain/terrain.ts
@@ -26,5 +26,7 @@ export {
   replaceVoxelEdits,
 } from "./terrain_edits.js";
 export { type VoxelEditSnapshot, type VoxelDelta } from "./voxel_edits/voxel_edit_types.js";
+export { type SdfBrush, type SdfBrushOp, type SdfBrushShape, applyBrushSdfToDensity, sampleBrushSdf } from "./sdf/sdf_brush.js";
+export { rasterizeSdfBrushToVoxelTransaction, type SdfBrushRasterizeInput, type SdfRasterBounds } from "./sdf/sdf_rasterizer.js";
 export { terrainWeights, paintMaterialAt, paintWeightsAt, type VertexPaint, PAINT_BLEND_CHANNELS, MATERIAL_PAINT_BAND, PAINT_FADE } from "./terrain_paint.js";
 export { meshChunk } from "./terrain_chunk_mesher.js";
