8b63cacf0b6619213d6277425455923b39761b32
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-06-26 07:15:58 +0400

    Track deep ocean surface in frame deps

--- tools/clod-poc/src/app/frame_loop/frame_loop_deps.ts
+++ tools/clod-poc/src/app/frame_loop/frame_loop_deps.ts
@@ -96,6 +96,7 @@ export interface FrameLoopVegetationDeps {
 
 export interface FrameLoopWaterWeatherDeps {
   waterController: WaterController;
+  deepOceanSurface: import("../../water/deep_ocean_surface.js").DeepOceanSurface | null;
   deepOceanMaterial: import("../../water/deep_ocean_material.js").DeepOceanMaterialHandle | null;
   waterField: import("../../water/waterField.js").WaterField;
   deepOceanConfig: import("../../terrain/border_coast_config.js").DeepOceanRenderConfig;
