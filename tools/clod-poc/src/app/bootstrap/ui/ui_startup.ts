1105b674f07f46fbaf919ed59d63fab663a98ba1
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-06-27 06:31:06 +0400

    Start prop editor UI

--- tools/clod-poc/src/app/bootstrap/ui/ui_startup.ts
+++ tools/clod-poc/src/app/bootstrap/ui/ui_startup.ts
@@ -4,6 +4,7 @@ import { runTerrainEditStartup } from "./terrain_edit_startup.js";
 import { runGuiStartup } from "./gui_startup.js";
 import { runTextureUiStartup } from "./texture_ui_startup.js";
 import { runSpellUiStartup } from "./spell_ui_startup.js";
+import { runPropEditUiStartup } from "./prop_edit_ui_startup.js";
 import { runProjectArchiveStartup } from "../project_archive_startup.js";
 import { applyImportedStateSideEffects } from "./imported_state_startup.js";
 import { runFrameLoopStartup } from "./frame_loop_startup.js";
@@ -22,6 +23,7 @@ export async function runUiStartup(input: UiStartupInput): Promise<void> {
   const gui = runGuiStartup(ctx, infoPanel);
   await runTextureUiStartup(ctx, infoPanel, gui, terrainEdit);
   runSpellUiStartup(ctx);
+  runPropEditUiStartup(ctx, gui.gui);
   runProjectArchiveStartup(ctx, infoPanel, terrainEdit);
   applyImportedStateSideEffects(ctx, infoPanel);
   runFrameLoopStartup(ctx, infoPanel, terrainEdit);
