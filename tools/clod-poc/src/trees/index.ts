ac27afe371eb5642bc15af007e4b98124de2b7e5
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-06-28 20:13:14 +0400

    Export GPU ring tree geometry helper

--- tools/clod-poc/src/trees/index.ts
+++ tools/clod-poc/src/trees/index.ts
@@ -6,6 +6,7 @@ export * from "./tree_ecology.js";
 export * from "./tree_material_bias.js";
 export * from "./tree_alpha_mask.js";
 export * from "./tree_geometry.js";
+export * from "./tree_gpu_ring_geometry.js";
 export * from "./tree_impostor_baker.js";
 export * from "./tree_impostor_material.js";
 export * from "./tree_impostor_octahedral.js";
