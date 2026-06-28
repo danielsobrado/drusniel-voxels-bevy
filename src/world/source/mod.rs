ecfb34bfc19837b5fc413ba462b13c58b12b0f07
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-06-28 16:21:55 +0400

    Export Bevy WorldSource config path

--- src/world/source/mod.rs
+++ src/world/source/mod.rs
@@ -7,6 +7,9 @@ pub mod world_source;
 
 pub use biome_region_field::{BiomeId, BiomeRegionField, BiomeRegionSample};
 pub use height_field::base_surface_height;
-pub use island_shape::{IslandMaskSample, IslandShapeConfig, sample_island_mask};
-pub use splat::{BiomeSplatSample, MaterialLayerId, sample_biome_splat};
-pub use world_source::{ProceduralWorldSource, TerrainFieldConfig, WorldSource, WorldSourceBounds, WorldSourceMetadata};
+pub use island_shape::{sample_island_mask, IslandMaskSample, IslandShapeConfig};
+pub use splat::{sample_biome_splat, BiomeSplatSample, MaterialLayerId};
+pub use world_source::{
+    ProceduralWorldSource, TerrainFieldConfig, WorldSource, WorldSourceBounds, WorldSourceMetadata,
+    WORLD_SOURCE_CONFIG_PATH,
+};
