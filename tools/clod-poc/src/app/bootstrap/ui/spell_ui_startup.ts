65637d377933f647fa3e506406f07e77227a39df
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-06-28 09:32:59 +0400

    Pass spell pose offsets to VFX controller

--- tools/clod-poc/src/app/bootstrap/ui/spell_ui_startup.ts
+++ tools/clod-poc/src/app/bootstrap/ui/spell_ui_startup.ts
@@ -2,25 +2,29 @@ import type { UiStartupContext } from "../ui_startup_context.js";
 import { createSpellMenu } from "../../../spells/spell_menu.js";
 import { defaultSpellConfig, type FireSpellVfxConfig } from "../../../spells/spell_config.js";
 import {
-  createSpellPoseResolver,
   createSpellVfxController,
   type SpellVfxMeshConfig,
 } from "../../../spells/spell_vfx_controller.js";
 import "../../../spells/spell_menu.css";
 
 function meshConfig(vfx: FireSpellVfxConfig): SpellVfxMeshConfig {
-  return { worldWidth: vfx.worldWidth, worldHeight: vfx.worldHeight, flameScale: vfx.flameScale };
+  return {
+    worldWidth: vfx.worldWidth,
+    worldHeight: vfx.worldHeight,
+    flameScale: vfx.flameScale,
+    handForwardM: vfx.handForwardM,
+    handRightM: vfx.handRightM,
+    handUpM: vfx.handUpM,
+  };
 }
 
 export function runSpellUiStartup(ctx: UiStartupContext): void {
   const config = defaultSpellConfig;
   const { scene, camera } = ctx.input;
 
-  const getPose = createSpellPoseResolver({ camera, vfx: config.fire.vfx });
   const controller = createSpellVfxController({
     scene,
     getCamera: () => camera,
-    getPose,
     fire: meshConfig(config.fire.vfx),
     water: meshConfig(config.water.vfx),
     air: meshConfig(config.air.vfx),
