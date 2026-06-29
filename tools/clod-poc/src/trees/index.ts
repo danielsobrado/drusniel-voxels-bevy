3e4cd3dbfdb7be4ee3d9ab697fdd8765b581bd04
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-06-29 14:31:45 +0400

    Export TREE-9 species expansion contract

--- tools/clod-poc/src/trees/index.ts
+++ tools/clod-poc/src/trees/index.ts
@@ -2,6 +2,7 @@ export * from "./tree_config.js";
 export * from "./tree_hash.js";
 export * from "./tree_noise.js";
 export * from "./tree_species.js";
+export * from "./tree_species_expansion.js";
 export * from "./tree_ecology.js";
 export * from "./tree_material_bias.js";
 export * from "./tree_alpha_mask.js";
