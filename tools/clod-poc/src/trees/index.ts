e657c198388352fca2afca686f9eacad9eb79290
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-06-29 14:17:50 +0400

    Export tree crown proxy node material

--- tools/clod-poc/src/trees/index.ts
+++ tools/clod-poc/src/trees/index.ts
@@ -6,6 +6,7 @@ export * from "./tree_ecology.js";
 export * from "./tree_material_bias.js";
 export * from "./tree_alpha_mask.js";
 export * from "./tree_crown_proxy_math.js";
+export * from "./tree_crown_proxy_node_material.js";
 export * from "./tree_geometry.js";
 export * from "./tree_gpu_ring_geometry.js";
 export * from "./tree_impostor_acceptance.js";
