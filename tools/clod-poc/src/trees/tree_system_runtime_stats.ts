ca942415d496723b5f42600e970ace0982bf1a57
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-06-30 06:01:51 +0400

    Compute TREE-10 hero fidelity stats in tree runtime

--- tools/clod-poc/src/trees/tree_system_runtime_stats.ts
+++ tools/clod-poc/src/trees/tree_system_runtime_stats.ts
@@ -1,11 +1,14 @@
 import type { TreeLod } from "./tree_config.js";
 import type { TreePatch, TreeStats } from "./tree_system_types.js";
+import { auditTreeHeroFidelity, createEmptyTreeHeroFidelityStats } from "./tree_hero_fidelity.js";
+import type { TreeGeometryMap } from "./tree_geometry.js";
 import { buildTreeSystemStats } from "./tree_system_stats.js";
 import type { TreeGpuRingRuntimeState } from "./tree_system_gpu_ring_runtime.js";
 import type { TreeImpostorStatus } from "./tree_system_types.js";
 
 export interface TreeRuntimeStatsInput {
   patches: readonly TreePatch[];
+  geometries: TreeGeometryMap;
   lodCounts: Record<TreeLod, number>;
   reportsGpuRingStats: boolean;
   gpuRing: TreeGpuRingRuntimeState;
@@ -18,6 +21,9 @@ export function buildTreeRuntimeStats(input: TreeRuntimeStatsInput): TreeStats {
   return buildTreeSystemStats({
     patches: input.patches,
     lodCounts: input.lodCounts,
+    heroFidelity: input.reportsGpuRingStats
+      ? createEmptyTreeHeroFidelityStats()
+      : auditTreeHeroFidelity({ patches: input.patches, geometries: input.geometries }),
     gpuRing: input.reportsGpuRingStats,
     gpuRingStats: input.gpuRing.stats,
     gpuVisibleCount: input.gpuRing.visibleCount,
