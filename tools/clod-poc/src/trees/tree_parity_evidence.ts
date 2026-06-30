712b4e84669917ba27847b7a3bdd96b888269a63
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-06-30 09:30:23 +0400

    Export TREE-11 acceptance evidence bridge

--- tools/clod-poc/src/trees/tree_parity_evidence.ts
+++ tools/clod-poc/src/trees/tree_parity_evidence.ts
@@ -3,3 +3,4 @@ export * from "./tree_parity_evidence_manifest.js";
 export * from "./tree_parity_evidence_validator.js";
 export * from "./tree_parity_evidence_commands.js";
 export * from "./tree_parity_evidence_report.js";
+export * from "./tree_parity_evidence_acceptance.js";
