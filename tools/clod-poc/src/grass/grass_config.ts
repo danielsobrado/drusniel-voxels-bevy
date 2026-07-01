253309f534db758eeff1761b7b3858b85c871b75
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-07-01 20:23:21 +0400

    Export grass config helpers from facade

--- tools/clod-poc/src/grass/grass_config.ts
+++ tools/clod-poc/src/grass/grass_config.ts
@@ -1,4 +1,5 @@
 export * from "./grass_config_types.js";
 export * from "./grass_config_defaults.js";
+export * from "./grass_config_readers.js";
 export * from "./grass_config_resolve.js";
 export * from "./grass_config_parsing.js";
