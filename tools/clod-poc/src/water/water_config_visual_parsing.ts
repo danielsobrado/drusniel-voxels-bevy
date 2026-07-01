4082e0ff29c565fe1582a13dc03895d423011d16
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-07-01 21:24:58 +0400

    Extract water visual config parsing

---
+++ tools/clod-poc/src/water/water_config_visual_parsing.ts
@@ -0,0 +1,92 @@
+import type { CausticsConfig } from "./causticsConfig.js";
+import type { WaterVisualConfig } from "./waterConfig.js";
+import {
+  readBoolean,
+  readColorTuple,
+  readNumber,
+  readNumberTuple,
+  recordFrom,
+} from "./water_config_readers.js";
+
+export function readWaterVisualConfig(value: unknown, defaults: WaterVisualConfig): WaterVisualConfig {
+  const visual = recordFrom(value);
+  const foam = recordFrom(visual.foam);
+  const fresnel = recordFrom(visual.fresnel);
+  const color = recordFrom(visual.color);
+  const refraction = recordFrom(visual.refraction);
+  const reflection = recordFrom(visual.reflection);
+
+  return {
+    shallowColor: readColorTuple(visual.shallow_color ?? visual.shallowColor, defaults.shallowColor),
+    deepColor: readColorTuple(visual.deep_color ?? visual.deepColor, defaults.deepColor),
+    foamColor: readColorTuple(visual.foam_color ?? visual.foamColor, defaults.foamColor),
+    alpha: readNumber(visual.alpha, defaults.alpha),
+    rippleCycle: readNumber(visual.ripple_cycle ?? visual.rippleCycle, defaults.rippleCycle),
+    fresnelPower: readNumber(visual.fresnel_power ?? visual.fresnelPower, defaults.fresnelPower),
+    rippleAmp: readNumber(visual.ripple_amp ?? visual.rippleAmp, defaults.rippleAmp),
+    rippleSpeed: readNumber(visual.ripple_speed ?? visual.rippleSpeed, defaults.rippleSpeed),
+    rippleScaleA: readNumber(visual.ripple_scale_a ?? visual.rippleScaleA, defaults.rippleScaleA),
+    rippleScaleB: readNumber(visual.ripple_scale_b ?? visual.rippleScaleB, defaults.rippleScaleB),
+    rippleStrengthA: readNumber(visual.ripple_strength_a ?? visual.rippleStrengthA, defaults.rippleStrengthA),
+    rippleStrengthB: readNumber(visual.ripple_strength_b ?? visual.rippleStrengthB, defaults.rippleStrengthB),
+    rippleLoopDistance: readNumber(visual.ripple_loop_distance ?? visual.rippleLoopDistance, defaults.rippleLoopDistance),
+    lakeBreeze: readNumberTuple(visual.lake_breeze ?? visual.lakeBreeze, defaults.lakeBreeze),
+    shoreFoamStart: readNumber(visual.shore_foam_start ?? visual.shoreFoamStart, defaults.shoreFoamStart),
+    shoreFoamEnd: readNumber(visual.shore_foam_end ?? visual.shoreFoamEnd, defaults.shoreFoamEnd),
+    maxDepthForColor: readNumber(visual.max_depth_for_color ?? visual.maxDepthForColor, defaults.maxDepthForColor),
+    foam: {
+      noiseScale: readNumber(foam.noise_scale ?? foam.noiseScale, defaults.foam.noiseScale),
+      shoreStrength: readNumber(foam.shore_strength ?? foam.shoreStrength, defaults.foam.shoreStrength),
+      riverStrength: readNumber(foam.river_strength ?? foam.riverStrength, defaults.foam.riverStrength),
+      speedStart: readNumber(foam.speed_start ?? foam.speedStart, defaults.foam.speedStart),
+      speedEnd: readNumber(foam.speed_end ?? foam.speedEnd, defaults.foam.speedEnd),
+      dropStart: readNumber(foam.drop_start ?? foam.dropStart, defaults.foam.dropStart),
+      dropEnd: readNumber(foam.drop_end ?? foam.dropEnd, defaults.foam.dropEnd),
+    },
+    fresnel: {
+      base: readNumber(fresnel.base, defaults.fresnel.base),
+      power: readNumber(fresnel.power, defaults.fresnel.power),
+      normalFlatten: readNumber(fresnel.normal_flatten ?? fresnel.normalFlatten, defaults.fresnel.normalFlatten),
+    },
+    color: {
+      depthScale: readNumber(color.depth_scale ?? color.depthScale, defaults.color.depthScale),
+      turbidity: readNumber(color.turbidity, defaults.color.turbidity),
+    },
+    refraction: {
+      enabled: readBoolean(refraction.enabled, defaults.refraction.enabled),
+      strength: readNumber(refraction.strength, defaults.refraction.strength),
+      depthValidationBias: readNumber(refraction.depth_validation_bias ?? refraction.depthValidationBias, defaults.refraction.depthValidationBias),
+      absorptionR: readNumber(refraction.absorption_r ?? refraction.absorptionR, defaults.refraction.absorptionR),
+      absorptionG: readNumber(refraction.absorption_g ?? refraction.absorptionG, defaults.refraction.absorptionG),
+      absorptionB: readNumber(refraction.absorption_b ?? refraction.absorptionB, defaults.refraction.absorptionB),
+      turbidityStrength: readNumber(refraction.turbidity_strength ?? refraction.turbidityStrength, defaults.refraction.turbidityStrength),
+      maxThickness: readNumber(refraction.max_thickness ?? refraction.maxThickness, defaults.refraction.maxThickness),
+    },
+    reflection: {
+      mode: reflection.mode === "ssr" ? "ssr" : "fake",
+      ssrEnabled: readBoolean(reflection.ssr_enabled ?? reflection.ssrEnabled, defaults.reflection.ssrEnabled),
+      maxSteps: readNumber(reflection.max_steps ?? reflection.maxSteps, defaults.reflection.maxSteps),
+      stepScale: readNumber(reflection.step_scale ?? reflection.stepScale, defaults.reflection.stepScale),
+      edgeFadeStart: readNumber(reflection.edge_fade_start ?? reflection.edgeFadeStart, defaults.reflection.edgeFadeStart),
+      edgeFadeEnd: readNumber(reflection.edge_fade_end ?? reflection.edgeFadeEnd, defaults.reflection.edgeFadeEnd),
+      skyFallbackStrength: readNumber(reflection.sky_fallback_strength ?? reflection.skyFallbackStrength, defaults.reflection.skyFallbackStrength),
+      terrainFallbackStrength: readNumber(reflection.terrain_fallback_strength ?? reflection.terrainFallbackStrength, defaults.reflection.terrainFallbackStrength),
+    },
+    depthWrite: readBoolean(visual.depth_write ?? visual.depthWrite, defaults.depthWrite),
+  };
+}
+
+export function readWaterCausticsConfig(value: unknown, defaults: CausticsConfig): CausticsConfig {
+  const caustics = recordFrom(value);
+  return {
+    enabled: readBoolean(caustics.enabled, defaults.enabled),
+    gain: readNumber(caustics.gain, defaults.gain),
+    depthFade: readNumber(caustics.depth_fade ?? caustics.depthFade, defaults.depthFade),
+    focalDepth: readNumber(caustics.focal_depth ?? caustics.focalDepth, defaults.focalDepth),
+    sunGateStart: readNumber(caustics.sun_gate_start ?? caustics.sunGateStart, defaults.sunGateStart),
+    sunGateEnd: readNumber(caustics.sun_gate_end ?? caustics.sunGateEnd, defaults.sunGateEnd),
+    flowAdvection: readNumber(caustics.flow_advection ?? caustics.flowAdvection, defaults.flowAdvection),
+    scale: readNumber(caustics.scale, defaults.scale),
+    speed: readNumber(caustics.speed, defaults.speed),
+  };
+}
