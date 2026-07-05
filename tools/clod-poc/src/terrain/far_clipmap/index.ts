6ecae0c6b65cd9deca7a0c25a6cb3f01f1027410
Daniel Sobrado <35833752+danielsobrado@users.noreply.github.com>
2026-07-05 21:01:07 +0400

    export default far clipmap source helper

--- tools/clod-poc/src/terrain/far_clipmap/index.ts
+++ tools/clod-poc/src/terrain/far_clipmap/index.ts
@@ -30,6 +30,7 @@ export type {
 
 export {
   createConservativeFarClipmapSource,
+  createDefaultFarClipmapSource,
   createFarClipmapSourceFromFarHeightProvider,
   createFarClipmapSourceFromProviderGetter,
   createFarClipmapSourceFromTerrainSampler,
